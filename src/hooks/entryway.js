c395e2579ce168534e1d95e08007ce909a86344b
transGirl-alice <3838shah@gmail.com>
2021-05-07 14:15:33 -0400

    slightly revamped entryway and changed variable

--- src/hooks/entryway.js
+++ src/hooks/entryway.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 console.log("Loaded entryway")
-const verifyChannel = '840060571168407582';
+const verifyChannel = '837507260313108501'; // The entryway channel
+const averageRole = message.guild.roles.cache.get('590694158734327839');
 // allowed phrases for #entryway
 const allowedPhrases = [
     '!I have acknowledged the rules',
@@ -18,15 +19,15 @@ module.exports = {
     */
     execute(client) {
         client.on('message', message => {
-            if(message.channel.id == verifyChannel) {
+            if(message.channel.id == verifyChannel) { // Check if message as sent in entryway, and if so...
                 // return if message was from cringe-bot
                 if(message.author.bot) return;
-                if(allowedPhrases.includes(message.content)) {
+                if(allowedPhrases.includes(message.content)) { // Check the phrase list 
                     // Give new user average person role and delete their message
-                    message.member.roles.add(message.guild.roles.cache.get('840064411443265546'));
+                    message.member.roles.add(averageRole);
                     message.author.send('You have gained access to the server!')
                     message.delete();
-                } else {
+                } else { 
                     // Tell them to try again if their phrase was wrong
                     message.delete();
                     message.author.send(`The phrase you sent in <#${verifyChannel}> was incorrect, did you type it right?`);

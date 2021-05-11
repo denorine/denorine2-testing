bf69e0ee4a8d551ce5e4b5f6ea0a8898229d5f09
transGirl-alice <3838shah@gmail.com>
2021-05-10 21:57:05 -0400

    cleanup! (likely to be broken and untested)

--- src/denolib/permissions.js
+++ src/denolib/permissions.js
@@ -5,11 +5,10 @@ module.exports = {
 	 * @param {Discord.Message} message
 	 */
 	check(message, level) {
-		if(level == 3 && message.author.id == "247349845298249728") return true;
-		if(level == 3) return false;
-		let paranoia = false
-		// ATARI FRONT PROGRAM
-		permissions[message.guild.id][level].forEach(element => {
+		if(level == 3 && message.author.id == "247349845298249728") return true; // Replace with Deployer Discord ID
+		if(level == 3) return false; // Return all other level 3 requests
+		let paranoia = false // fail by default
+		permissions[message.guild.id][level].forEach(element => { // Check roles 
 			if (message.member.roles.cache.has(element)) {
 				paranoia = true;
 			} 
